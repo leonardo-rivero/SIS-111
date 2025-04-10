2b8edd6d7e89a25417b6818bdbee5f93cd8a8b57
leonardo-rivero <leonardo.rivero.l@ucb.edu.bo>
2025-04-10 09:50:17 -0400

    Add files via upload
    operadores aritmeticos

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,31 @@
+
+#crear variable
+saludos="hola mundo!!!"
+print(saludos)
+
+#suma
+num1=123
+num2=321
+suma=num1+num2
+print(suma)
+
+#suma de palabras
+palabra1=" hola "
+palabra2="mundo "
+sumapalabra=palabra1+palabra2
+print(sumapalabra)
+#resta
+num3=12
+num4=46
+resta=num3-num4
+print(resta)
+#multipliocacion
+num5=78
+num6=96
+multiplicacion=num5*num6
+print(multiplicacion)
+#DIVISION
+num7=89
+num8=23
+division=num7/num8
+print(division)
